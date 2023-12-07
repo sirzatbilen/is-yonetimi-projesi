7446e6de694b05703d3a1e409b84860c4f8826ae
sirzat-bilen <sirzat.bilen.9079@gmail.com>
2023-12-07 05:37:07 +0300

    client devam

--- api/server.js
+++ api/server.js
@@ -4,8 +4,9 @@ const cors = require("cors");
 const customerRouter = require("./customers/CustomerRouter");
 const representativeRouter = require("./representatives/RepresentativesRouter");
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 
 app.use("/api/customers", customerRouter);
 app.use("/api/representatives", representativeRouter);
